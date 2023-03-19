9261dfae29a82ac14b87145a027f2353aa89896a
Gianfranco <gianjrs.98@gmail.com>
2023-03-19 23:04:00 +0100

    fic auth redirect

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -14,7 +14,7 @@ export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
     
   
-    if(!session){
+    if(session){
       return {
         redirect: {
         destination: '/auth',
