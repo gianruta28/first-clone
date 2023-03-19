3b92673ec659223725b5e6b381b8957f5f1cc184
Gianfranco <gianjrs.98@gmail.com>
2023-03-19 21:50:12 +0100

    testing issue11

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -4,7 +4,8 @@ import { getSession } from "next-auth/react";
 import prismadb from '@/libs/prismadb';
 
 const serverAuth = async (req: NextApiRequest) => {
-  const session = await getSession({ req });
+  const session = await getSession({req});
+
 
   if (!session?.user?.email) {
     throw new Error('Not signed in');
